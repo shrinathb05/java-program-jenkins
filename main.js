ffb55f3d3ee5f507c905250337361f50cf232d29
shrinath <shrinath05@gmail.com>
2024-01-19 18:51:50 +0530

    Node file added

---
+++ main.js
@@ -0,0 +1,16 @@
+// hello_node_extended.js
+
+// Print a greeting message
+console.log("Hello, Node.js!");
+
+// Define a variable
+let favoriteNumber = 42;
+
+// Print the variable value
+console.log("My favorite number is:", favoriteNumber);
+
+// Perform a simple calculation
+let doubleNumber = favoriteNumber * 2;
+
+// Print the result of the calculation
+console.log("Twice my favorite number is:", doubleNumber);
